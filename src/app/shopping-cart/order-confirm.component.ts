02dce34337fcfee405931242e99fe927c7370169
antony2 <antonyrokr@gmail.com>
2019-06-14 09:42:02 +0530

    Order-Confirm added

---
+++ src/app/shopping-cart/order-confirm.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'order-confirm',
+  templateUrl: 'order-confirm.component.html'
+})
+export class OrderConfirmComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
